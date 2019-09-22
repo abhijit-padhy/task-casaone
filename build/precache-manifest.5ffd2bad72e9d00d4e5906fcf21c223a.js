ad3dad80c58116ff6e0a25715cef72701b0ce3d1
abhijit <abhijitp@orowealth.com>
2019-09-23 03:22:37 +0530

    first commit

---
+++ build/precache-manifest.5ffd2bad72e9d00d4e5906fcf21c223a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8d367a918a937a94a2902d6bae83808",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21a4f16876599ebb7215",
+    "url": "/static/css/main.32ed8ec1.chunk.css"
+  },
+  {
+    "revision": "8c4753e00b0aac2f18ae",
+    "url": "/static/js/2.54077bbb.chunk.js"
+  },
+  {
+    "revision": "21a4f16876599ebb7215",
+    "url": "/static/js/main.0a9cd92b.chunk.js"
+  },
+  {
+    "revision": "7458a59f211050cc8a32",
+    "url": "/static/js/runtime~main.4dd16644.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
